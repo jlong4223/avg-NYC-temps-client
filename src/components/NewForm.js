d74df1ed30307f3d03ff31fc297384e6649beb3e
jared Long <jlong4223@gmail.com>
2021-01-04 16:23:27 -0600

    edits form state

--- src/components/NewForm.js
+++ src/components/NewForm.js
@@ -3,7 +3,7 @@ import {useState} from 'react'
 
 
 const NewForm = (props)=>{
-
+// TODO fix state and form submit
     const [tempState, setTempState]= useState({
         average_high_f: 0,
         average_low_f: 0,
@@ -17,13 +17,30 @@ const NewForm = (props)=>{
             [e.target.name]: e.target.value
         }))
     }
-// TODO edit the type within the input
+
+    function submitTemp(e, formInputs){
+        e.preventDefault()
+        console.log({tempState})
+        fetch('/temperatures', {
+            body: JSON.stringify(formInputs),
+            method: 'POST', 
+            headers: {
+                "Accept": "application/json, text/plain, */*",
+                "Content-type": "application/json"
+            }
+        }).then(createdTemp => createdTemp.json())
+        .then(jsonedTemp => {setTempState(prevState => ({ tempState:
+            [jsonedTemp, ...prevState]
+        }))})
+        .catch(error => console.log(error))
+    }
+
     return(
-        <form>
+        <form onSubmit={submitTemp}>
             <legend>Add new temperature</legend>
-            <input placeholder="average high" type="number" name="average_high_f" value={tempState.average_high_f} onChange={handleTempChange}/>
-            <input placeholder="average low" type="number" name="average_low_f" value={tempState.average_low_f} onChange={handleTempChange}/>
-            <input placeholder="month" type="text" name="month" value={tempState.month}/>
+            <input placeholder="average high" type="number" name="average_high_f" value={tempState.average_high_f} onChange={handleTempChange} required/>
+            <input placeholder="average low" type="number" name="average_low_f" value={tempState.average_low_f} onChange={handleTempChange} required/>
+            <input placeholder="month" type="text" name="month" value={tempState.month} onChange={handleTempChange} required/>
             <button type='submit'>Add</button>
         </form>
     )
